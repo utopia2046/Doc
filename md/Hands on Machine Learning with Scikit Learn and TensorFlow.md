fb59cea6805313638a8474c130f354f18dc4c0f1
utopia <utopia2046@hotmail.com>
2023-03-08 17:45:12 +0800

    update 230308

--- md/Hands on Machine Learning with Scikit Learn and TensorFlow.md
+++ md/Hands on Machine Learning with Scikit Learn and TensorFlow.md
@@ -131,7 +131,81 @@ for train_index, test_index in split.split(housing, housing["income_cat"]):
     strat_train_set = housing.loc[train_index]
     strat_test_set = housing.loc[test_index]
 
+# drop labels for training set
+data = strat_train_set.drop("median_house_value", axis=1)
+data_labels = strat_train_set["median_house_value"].copy()
+
+# fill missing value using sklearn imputer
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(strategy="median")
+housing_num = data.drop('ocean_proximity', axis=1) # drop non-number properties
+imputer.fit(housing_num)
+# transform training set
+X = imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns, index=data.index)
+
+# transform catogorical property to OneHot encoding
+from sklearn.preprocessing import OneHotEncoder
+cat_encoder = OneHotEncoder(sparse=False)
+housing_cat_1hot = cat_encoder.fit_transform(housing_cat)
+
+# number pipeline: fill missing value + add extra feature + normalization
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+num_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy="median")),
+        ('attribs_adder', FunctionTransformer(add_extra_features, validate=False)),
+        ('std_scaler', StandardScaler()),
+    ])
+# full pipeline: number pipeline + onehot encoder text attributes
+from sklearn.compose import ColumnTransformer
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+full_pipeline = ColumnTransformer([
+        ("num", num_pipeline, num_attribs),
+        ("cat", OneHotEncoder(), cat_attribs),
+    ])
+housing_prepared = full_pipeline.fit_transform(data)
+
+# fit random forest
+from sklearn.ensemble import RandomForestRegressor
+forest_reg = RandomForestRegressor(n_estimators=10, random_state=42)
+forest_reg.fit(housing_prepared, housing_labels)
+housing_predictions = forest_reg.predict(housing_prepared)
+forest_mse = mean_squared_error(housing_labels, housing_predictions)
+forest_rmse = np.sqrt(forest_mse) # 21933.31414779769
+# cross validation
+from sklearn.model_selection import cross_val_score
+forest_scores = cross_val_score(forest_reg, housing_prepared, housing_labels,
+                                scoring="neg_mean_squared_error", cv=10)
+forest_rmse_scores = np.sqrt(-forest_scores)
+
+# use grid search to tune hyper-parameters (or RandomizedSearch for many hyper-parameters)
+from sklearn.model_selection import GridSearchCV
+param_grid = [
+    # try 12 (3×4) combinations of hyperparameters
+    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+    # then try 6 (2×3) combinations with bootstrap set as False
+    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+  ]
+forest_reg = RandomForestRegressor(random_state=42)
+# train across 5 folds, that's a total of (12+6)*5=90 rounds of training
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+                           scoring='neg_mean_squared_error', return_train_score=True)
+grid_search.fit(housing_prepared, housing_labels)
+grid_search.best_estimator_
 ```
+
+Ref:
+
+- <http://contrib.scikit-learn.org/category_encoders/#>
+- <https://blog.csdn.net/sinat_26917383/article/details/107851162>
+
+Tips: Show all properties of an object in IPython
+
+1. vars(obj) # show all attributes
+2. dir(obj)  # show all attributes and methods, including inherited ones
+3. help(obj) # show help document
 
 <!---
 TBD below:
--- script/handsonml-houseprice.py
+++ script/handsonml-houseprice.py
@@ -1,10 +1,16 @@
 import os
 import tarfile
 import urllib.request
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from sklearn.preprocessing import FunctionTransformer
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+PROJECT_ROOT_DIR = os.path.curdir
 
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     os.makedirs(housing_path, exist_ok=True)
@@ -14,13 +20,9 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
 
-import pandas as pd
-
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
-
-import numpy as np
 
 # For illustration only. Sklearn has train_test_split()
 def split_train_test(data, test_ratio):
@@ -29,6 +31,45 @@ def split_train_test(data, test_ratio):
     test_indices = shuffled_indices[:test_set_size]
     train_indices = shuffled_indices[test_set_size:]
     return data.iloc[train_indices], data.iloc[test_indices]
+
+# Download the California image
+def download_california_image():
+    images_path = os.path.join(PROJECT_ROOT_DIR, "images", "end_to_end_project")
+    os.makedirs(images_path, exist_ok=True)
+    DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+    filename = "california.png"
+    print("Downloading", filename)
+    url = DOWNLOAD_ROOT + "images/end_to_end_project/" + filename
+    urllib.request.urlretrieve(url, os.path.join(images_path, filename))
+
+# Show data scatter plot on california map
+def plot_california_housing_data(data):
+    california_img=mpimg.imread(PROJECT_ROOT_DIR + '/images/end_to_end_project/california.png')
+    ax = data.plot(kind="scatter", x="longitude", y="latitude", figsize=(10,7),
+                        s=data['population']/100, label="Population",
+                        c="median_house_value", cmap=plt.get_cmap("jet"),
+                        colorbar=False, alpha=0.4,
+                        )
+    plt.imshow(california_img, extent=[-124.55, -113.80, 32.45, 42.05], alpha=0.5,
+            cmap=plt.get_cmap("jet"))
+    plt.ylabel("Latitude", fontsize=14)
+    plt.xlabel("Longitude", fontsize=14)
+
+    prices = data["median_house_value"]
+    tick_values = np.linspace(prices.min(), prices.max(), 11)
+    cbar = plt.colorbar()
+    cbar.ax.set_yticklabels(["$%dk"%(round(v/1000)) for v in tick_values], fontsize=14)
+    cbar.set_label('Median House Value', fontsize=16)
+
+def add_extra_features(X, add_bedrooms_per_room=True):
+    rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+    population_per_household = X[:, population_ix] / X[:, household_ix]
+    if add_bedrooms_per_room:
+        bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+        return np.c_[X, rooms_per_household, population_per_household,
+                     bedrooms_per_room]
+    else:
+        return np.c_[X, rooms_per_household, population_per_household]
 
 """
 # fetch data
@@ -45,7 +86,6 @@ housing["ocean_proximity"].value_counts()
 
 # plot histogram for each column (feature)
 %matplotlib
-import matplotlib.pyplot as plt
 housing.hist(bins=50, figsize=(20,15))
 
 # split train and test set
@@ -63,4 +103,149 @@ split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 for train_index, test_index in split.split(housing, housing["income_cat"]):
     strat_train_set = housing.loc[train_index]
     strat_test_set = housing.loc[test_index]
+
+# copy train set
+data = strat_train_set.copy()
+data.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+    s=data["population"]/100, label="population", figsize=(10,7),
+    c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,
+    sharex=False)
+plt.legend()
+
+download_california_image()
+plot_california_housing_data(data)
+
+# exploration: examine properties correlation
+corr_matrix = data.corr()
+corr_matrix["median_house_value"].sort_values(ascending=False)
+attributes = ["median_house_value", "median_income", "total_rooms",
+              "housing_median_age"]
+from pandas.plotting import scatter_matrix
+scatter_matrix(housing[attributes], figsize=(12, 8))
+
+# create some new combined properties
+data["rooms_per_household"] = data["total_rooms"]/data["households"]
+data["bedrooms_per_room"] = data["total_bedrooms"]/data["total_rooms"]
+data["population_per_household"]=data["population"]/data["households"]
+data.plot(kind="scatter", x="rooms_per_household", y="median_house_value",
+             alpha=0.2)
+plt.axis([0, 5, 0, 520000])
+
+# drop labels for training set
+data = strat_train_set.drop("median_house_value", axis=1)
+data_labels = strat_train_set["median_house_value"].copy()
+
+# fill missing value with median value
+median = data["total_bedrooms"].median()
+data["total_bedrooms"].fillna(median, inplace=True)
+
+# fill missing value using sklearn imputer
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(strategy="median")
+housing_num = data.drop('ocean_proximity', axis=1) # drop non-number properties
+imputer.fit(housing_num)
+# transform training set
+X = imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns, index=data.index)
+
+# transform text properties to numbers
+from sklearn.preprocessing import OrdinalEncoder
+housing_cat = data[['ocean_proximity']]
+ordinal_encoder = OrdinalEncoder()
+housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
+# transform catogorical property to OneHot encoding
+from sklearn.preprocessing import OneHotEncoder
+cat_encoder = OneHotEncoder(sparse=False)
+housing_cat_1hot = cat_encoder.fit_transform(housing_cat)
+# or LabelBinarizer
+from sklearn.preprocessing import LabelBinarizer
+encoder = LabelBinarizer()
+housing_cat_1hot = encoder.fit_transform(housing_cat)
+
+# add combined feature based on other features
+attr_adder = FunctionTransformer(add_extra_features, validate=False,
+                                 kw_args={"add_bedrooms_per_room": False})
+housing_extra_attribs = attr_adder.fit_transform(data.values)
+housing_extra_attribs = pd.DataFrame(
+    housing_extra_attribs,
+    columns=list(housing.columns)+["rooms_per_household", "population_per_household"],
+    index=housing.index)
+
+# number pipeline: fill missing value + add extra feature + normalization
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+num_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy="median")),
+        ('attribs_adder', FunctionTransformer(add_extra_features, validate=False)),
+        ('std_scaler', StandardScaler()),
+    ])
+# full pipeline: number pipeline + onehot encoder text attributes
+from sklearn.compose import ColumnTransformer
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+full_pipeline = ColumnTransformer([
+        ("num", num_pipeline, num_attribs),
+        ("cat", OneHotEncoder(), cat_attribs),
+    ])
+housing_prepared = full_pipeline.fit_transform(data)
+
+# fit linear regression model
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared, housing_labels)
+from sklearn.metrics import mean_squared_error
+housing_predictions = lin_reg.predict(housing_prepared)
+lin_mse = mean_squared_error(housing_labels, housing_predictions)
+lin_rmse = np.sqrt(lin_mse) # 68628.19819848922, big error
+from sklearn.metrics import mean_absolute_error
+lin_mae = mean_absolute_error(housing_labels, housing_predictions) # 49439.89599001897, big error
+
+# fit decision tree
+from sklearn.tree import DecisionTreeRegressor
+tree_reg = DecisionTreeRegressor(random_state=42)
+tree_reg.fit(housing_prepared, housing_labels)
+housing_predictions = tree_reg.predict(housing_prepared)
+tree_mse = mean_squared_error(housing_labels, housing_predictions)
+tree_rmse = np.sqrt(tree_mse) # 0, overfitting on training set
+
+# cross validation
+from sklearn.model_selection import cross_val_score
+scores = cross_val_score(tree_reg, housing_prepared, housing_labels,
+                         scoring="neg_mean_squared_error", cv=10)
+tree_rmse_scores = np.sqrt(-scores)
+
+# fit random forest
+from sklearn.ensemble import RandomForestRegressor
+forest_reg = RandomForestRegressor(n_estimators=10, random_state=42)
+forest_reg.fit(housing_prepared, housing_labels)
+housing_predictions = forest_reg.predict(housing_prepared)
+forest_mse = mean_squared_error(housing_labels, housing_predictions)
+forest_rmse = np.sqrt(forest_mse) # 21933.31414779769
+forest_scores = cross_val_score(forest_reg, housing_prepared, housing_labels,
+                                scoring="neg_mean_squared_error", cv=10)
+forest_rmse_scores = np.sqrt(-forest_scores)
+
+# fit SVR with linear core
+from sklearn.svm import SVR
+svm_reg = SVR(kernel="linear")
+svm_reg.fit(housing_prepared, housing_labels)
+housing_predictions = svm_reg.predict(housing_prepared)
+svm_mse = mean_squared_error(housing_labels, housing_predictions)
+svm_rmse = np.sqrt(svm_mse)
+
+# tried several models, random forest has best score
+# now use grid search to tune hyperparameters
+from sklearn.model_selection import GridSearchCV
+param_grid = [
+    # try 12 (3×4) combinations of hyperparameters
+    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+    # then try 6 (2×3) combinations with bootstrap set as False
+    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+  ]
+forest_reg = RandomForestRegressor(random_state=42)
+# train across 5 folds, that's a total of (12+6)*5=90 rounds of training
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+                           scoring='neg_mean_squared_error', return_train_score=True)
+grid_search.fit(housing_prepared, housing_labels)
+
 """
