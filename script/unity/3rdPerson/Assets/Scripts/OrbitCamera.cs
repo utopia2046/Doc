1e2ef9d48c2b87e116902a87ac87dfd65af79640
utopia <utopia2046@hotmail.com>
2024-04-15 18:46:57 +0800

    fix tabs

--- script/unity/3rdPerson/Assets/Scripts/OrbitCamera.cs
+++ script/unity/3rdPerson/Assets/Scripts/OrbitCamera.cs
@@ -6,37 +6,37 @@ using UnityEngine;
 
 public class OrbitCamera : MonoBehaviour
 {
-	[SerializeField] Transform target;
+    [SerializeField] Transform target;
 
-	public float rotSpeed = 1.5f;
+    public float rotSpeed = 1.5f;
 
-	private float rotY;
-	private Vector3 offset;
+    private float rotY;
+    private Vector3 offset;
 
-	// Use this for initialization
-	void Start()
-	{
-		rotY = transform.eulerAngles.y;
-		offset = target.position - transform.position;
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rotY = transform.eulerAngles.y;
+        offset = target.position - transform.position;
+    }
 
-	// Update is called once per frame
-	void LateUpdate()
-	{
-		float horInput = Input.GetAxis("Horizontal");
-		if (!Mathf.Approximately(horInput, 0))
-		{
-			rotY += horInput * rotSpeed;
-		}
-		else
-		{
-			rotY += Input.GetAxis("Mouse X") * rotSpeed * 3;
-		}
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float horInput = Input.GetAxis("Horizontal");
+        if (!Mathf.Approximately(horInput, 0))
+        {
+            rotY += horInput * rotSpeed;
+        }
+        else
+        {
+            rotY += Input.GetAxis("Mouse X") * rotSpeed * 3;
+        }
 
-		Quaternion rotation = Quaternion.Euler(0, rotY, 0);
-		// move camera position with relative offset fixed
-		transform.position = target.position - (rotation * offset);
-		// no matter where the camera is relative to the target, always face the target
-		transform.LookAt(target);
-	}
+        Quaternion rotation = Quaternion.Euler(0, rotY, 0);
+        // move camera position with relative offset fixed
+        transform.position = target.position - (rotation * offset);
+        // no matter where the camera is relative to the target, always face the target
+        transform.LookAt(target);
+    }
 }
