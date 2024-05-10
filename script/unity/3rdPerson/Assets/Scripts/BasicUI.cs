68303c6ad0cec2c54663bb763cc26927ff12715a
utopia <utopia2046@hotmail.com>
2024-05-10 16:51:58 +0800

    fix inventory display

--- script/unity/3rdPerson/Assets/Scripts/BasicUI.cs
+++ script/unity/3rdPerson/Assets/Scripts/BasicUI.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class BasicUI : MonoBehaviour
 {
-    int posX = 10;
-    int posY = 10;
-    int width = 100;
-    int height = 30;
-    int buffer = 10;
-    int fontSize = 18;
+    [SerializeField] int fontSize = 18;
     private GUIStyle currentStyle = null;
 
     void OnGUI()
     {
+        int posX = 10;
+        int posY = 10;
+        int width = 100;
+        int height = 30;
+        int buffer = 10;
+
         List<string> itemList = Managers.Inventory.GetItemList();
-        //InitStyles(fontSize);
+
+        if (currentStyle == null)
+        {
+            currentStyle = MakeStyle(fontSize);
+        }
 
         if (itemList.Count == 0)
         {
-            GUI.Box(new Rect(posX, posY, width, height), "No Items"); //, currentStyle
+            GUI.Box(new Rect(posX, posY, width, height), "No Items", currentStyle);
             return;
         }
 
@@ -27,37 +32,27 @@ public class BasicUI : MonoBehaviour
         {
             int count = Managers.Inventory.GetItemCount(item);
             Texture2D image = Resources.Load<Texture2D>($"Icons/{item}");
-            GUI.Box(new Rect(posX, posY, width, height), new GUIContent($"({count})", image)); //, currentStyle
+            GUI.Box(new Rect(posX, posY, width, height), new GUIContent($"({count})", image), currentStyle);
             posX += width + buffer;
         }
     }
 
-    private void InitStyles(int fontSize)
+    private GUIStyle MakeStyle(int fontSize)
     {
-        if (currentStyle == null)
-        {
-            //Texture2D background = new Texture2D(1, 1, TextureFormat.RGBAFloat, false);
-            //background.SetPixel(0, 0, new Color(0f, 0f, 0f, 0.5f));
-            //background.Apply();
+        GUIStyle style = new GUIStyle(GUI.skin.box);
+        style.normal.background = MakeTexture(new Color(0f, 0f, 0f, 0.5f));
+        style.normal.textColor = Color.white;
+        style.fontSize = fontSize;
+        style.fontStyle = FontStyle.Bold;
 
-            currentStyle = new GUIStyle(); // new GUIStyle(GUI.skin.box)
-            //currentStyle.normal.background = MakeTex(2, 2, new Color(0f, 0f, 0f, 0.5f)); //background;
-            currentStyle.normal.textColor = Color.white;
-            currentStyle.fontSize = fontSize;
-            currentStyle.fontStyle = FontStyle.Bold;
-        }
+        return style;
     }
 
-    private Texture2D MakeTex(int width, int height, Color col)
+    private Texture2D MakeTexture(Color backgroundColor)
     {
-        Color[] pix = new Color[width * height];
-        for (int i = 0; i < pix.Length; ++i)
-        {
-            pix[i] = col;
-        }
-        Texture2D result = new Texture2D(width, height);
-        result.SetPixels(pix);
-        result.Apply();
-        return result;
+        Texture2D background = new Texture2D(1, 1, TextureFormat.RGBAFloat, false);
+        background.SetPixel(0, 0, backgroundColor);
+        background.Apply();
+        return background;
     }
 }
