697a2999fff63c621eef5c452906491c88c280f2
utopia <utopia2046@hotmail.com>
2024-11-18 19:47:34 +0800

    implement robot turn around on collision

--- script/unity/DotGame/Assets/Scripts/Robot.cs
+++ script/unity/DotGame/Assets/Scripts/Robot.cs
@@ -7,9 +7,9 @@ public class Robot : MonoBehaviour
     public enum Directions
     {
         Up,
+        Right,
         Down,
-        Left,
-        Right
+        Left
     }
 
     public float speed;
@@ -49,5 +49,35 @@ public class Robot : MonoBehaviour
 
         rb.velocity = dirVector.normalized * speed;
         animator.SetFloat("Speed", rb.velocity.magnitude);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // retreat back a little to avoid stuck after turning
+        Vector2 newPosition = new Vector2(
+            transform.position.x - dirVector.x * 0.1f,
+            transform.position.y - dirVector.y * 0.1f);
+        rb.MovePosition(newPosition);
+        //direction = (Directions)(((int)direction + 1) % 4);
+        // on collision, turn clockwise and switch animation
+        switch (direction)
+        {
+            case Directions.Up:
+                direction = Directions.Right;
+                animator.Play("Robot_MoveRight");
+                break;
+            case Directions.Right:
+                direction = Directions.Down;
+                animator.Play("Robot_MoveDown");
+                break;
+            case Directions.Down:
+                direction = Directions.Left;
+                animator.Play("Robot_MoveLeft");
+                break;
+            case Directions.Left:
+                direction = Directions.Up;
+                animator.Play("Robot_MoveUp");
+                break;
+        }
     }
 }
