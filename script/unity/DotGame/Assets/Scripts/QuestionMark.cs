c2768ff8c20dedc41611847ee48daab78b8520cc
utopia <utopia2046@hotmail.com>
2024-12-30 19:49:14 +0800

    add QuestionMark ding sound on collected

--- script/unity/DotGame/Assets/Scripts/QuestionMark.cs
+++ script/unity/DotGame/Assets/Scripts/QuestionMark.cs
@@ -4,14 +4,49 @@ using UnityEngine;
 
 public class QuestionMark : MonoBehaviour
 {
+    public float disappearTime = 1.0f;
+    public float disappearSpeed = 1.0f;
+    private AudioSource ding;
+    private float deathTimer;
+
+    void Start()
+    {
+        ding = GetComponent<AudioSource>();
+        deathTimer = disappearTime;
+    }
+
+    void Update()
+    {
+        if (deathTimer < disappearTime)
+        {
+            deathTimer -= Time.deltaTime;
+            float shrink = 1.0f - disappearSpeed * Time.deltaTime;
+            transform.localScale = new Vector3(
+                shrink * transform.localScale.x,
+                shrink * transform.localScale.y,
+                transform.localScale.z);
+            if (deathTimer < 0.0f)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (deathTimer < disappearTime)
+        {
+            Physics2D.IgnoreCollision(collision.collider, gameObject.GetComponent<Collider2D>());
+            return;
+        }
+
         if (collision.gameObject.tag == "Player")
         {
             int randomscore = Random.Range(10, 101);
             Scoring.gameScore += randomscore;
-            //Destroy(gameObject);
-            gameObject.SetActive(false);
+            ding.Play();
+            //gameObject.SetActive(false);
+            deathTimer -= 0.01f;
         }
     }
 }
