754165da195cdcae9a82503d5739971617a3b2af
utopia <utopia2046@hotmail.com>
2025-03-20 20:24:27 +0800

    finish card game

---
+++ script/unity/TankFight/Assets/Scripts/CardGame/CustomNodes/RandomBinaryNode.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class RandomBinaryNode : BTNode
+{
+    public override NodeStates Evaluate()
+    {
+        var roll = Random.Range(0, 2);
+        return (roll == 0 ? NodeStates.SUCCESS : NodeStates.FAILURE);
+    }
+}
